378dc79c14922808eaa880f7d1047aa2ef4a4d6d
Hiroyuki Ota <hota@flywheel.jp>
2024-12-31 12:36:38 +0900

    Mutate the genome.

--- examples/neat.py
+++ examples/neat.py
@@ -1,6 +1,11 @@
 from dataclasses import dataclass
+import dataclasses
+from functools import partial
+import math
+import os
 import jax
 import numpy as np
+from evojax import util
 from evojax.algo.base import NEAlgorithm
 from evojax.policy.base import PolicyNetwork, PolicyState
 from evojax.task.base import TaskState
@@ -234,6 +239,25 @@ class NEATConfig:
     # Population size.
     pop_size: int
 
+    # Rate of the population to keep.
+    rate_keep_best: float = 0.2
+
+    # ------------------------------
+    # Structure mutation parameters.
+    # ------------------------------
+    # Rate of adding a node.
+    prob_add_node: float = 0.1
+
+    # Rate of adding a connection.
+    prob_add_connection: float = 0.2
+
+    # ------------------------------
+    # Attribute mutation parameters.
+    # ------------------------------
+    prob_update_activation: float = 0.1
+    prob_update_conn_weights: float = 0.1
+    prob_update_conn_enabled: float = 0.1
+
 
 def ask_fn(neat_config: NEATConfig):
     """Generate a new population."""
@@ -243,6 +267,7 @@ def ask_fn(neat_config: NEATConfig):
     # Repeat the genome for the population size.
     return jax.tree.map(lambda x: jnp.tile(x, (neat_config.pop_size, 1)), genome)
 
+
 # Type alias for the genome population.
 # To make it easier to understand the genome is only a single genome or a population of genomes.
 GenomePopulation = Genome
@@ -250,7 +275,325 @@ GenomePopulation = Genome
 
 def tell_fn(fitness: jax.typing.ArrayLike, pops: GenomePopulation) -> Genome:
     maxi = jax.lax.argmax(fitness, 0, jnp.int32)
-    return jax.tree.map(lambda leaf: leaf[maxi], pops)
+    best_params = jax.tree.map(lambda leaf: leaf[maxi], pops)
+
+    # Calculate next generation.
+
+    return best_params
+
+
+@jax.jit
+def pick_one(key: jax.Array, mask: jax.Array) -> int:
+    """Pick up one index where the mask is truefp.
+
+    Args:
+        mask (jax.Array): The mask. 1-d array of bool.
+    Returns:
+        jax.Array: The selected index.
+    """
+    idx = jnp.arange(mask.shape[0])
+    # Filter the indices where mask is True.
+    filtered = jnp.sort(jnp.where(mask, idx, -1), descending=True)
+    # Pick up one index from the filtered indices.
+    chosen = jax.random.randint(key, (1,), 0, mask.sum())
+    return filtered[chosen][0]  # type: ignore
+
+
+# @partial(jax.jit, static_argnums=(0,))
+def calculate_depth(
+    config: Config,
+    conn_in: jax.Array,
+    conn_out: jax.Array,
+) -> jax.Array:
+    """Calculate the depth of the nodes.
+
+    Returns the maximum depth of the nodes or -1 if
+    - the depth is greater than the max_depth
+    - or there is a cycle in the network.
+    """
+    depth = jax.lax.pad(
+        jnp.ones(config.input_dim, dtype=jnp.int32),
+        0,
+        [(0, config.max_nodes - config.input_dim, 0)],
+    )
+    for _ in range(config.max_depth + 1):
+        # Update the depth of the nodes.
+        depth = depth.at[conn_out].set(
+            jnp.where(
+                conn_out >= 0,
+                jnp.maximum(depth[conn_out], depth[conn_in] + 1),
+                depth[conn_out],
+            )
+        )
+
+    max_depth = jnp.max(depth)
+    return jnp.where(max_depth > config.max_depth, -1, max_depth)
+
+
+def mutate_add_node(
+    config: Config, genome: Genome, key: jax.Array, next_edge_id: jax.Array
+) -> tuple[Genome, jax.Array]:
+    """Add nodes to the genome. This function is not jit-compiled."""
+    conn_idx = pick_one(key, genome.conn_enabled)
+
+    # Get the selected connection values
+    in_node = genome.conn_in[conn_idx]
+    out_node = genome.conn_out[conn_idx]
+    weight = genome.conn_weights[conn_idx]
+
+    # Add a new node
+    new_node_id = genome.node_ids.max() + 1
+
+    node_ids = genome.node_ids.at[new_node_id].set(new_node_id)
+    node_activation = genome.node_activation.at[new_node_id].set(0)
+
+    # Disable the connection
+    conn_enabled = genome.conn_enabled.at[conn_idx].set(False)
+
+    # Get the new connection indexes
+    new_edge1_idx = jnp.count_nonzero(genome.conn_ids >= 0)
+    new_edge_idx = jnp.array([new_edge1_idx, new_edge1_idx + 1])
+
+    conn_ids = genome.conn_ids.at[new_edge_idx].set(
+        jnp.array([next_edge_id, next_edge_id + 1])
+    )
+    conn_in = genome.conn_in.at[new_edge_idx].set(jnp.array([in_node, new_node_id]))
+    conn_out = genome.conn_out.at[new_edge_idx].set(jnp.array([new_node_id, out_node]))
+    conn_weights = genome.conn_weights.at[new_edge_idx].set(jnp.array([1.0, weight]))
+    conn_enabled = conn_enabled.at[new_edge_idx].set(jnp.array([True, True]))
+
+    return (
+        Genome(
+            node_ids=jnp.where(
+                new_node_id >= config.max_nodes, genome.node_ids, node_ids
+            ),
+            node_activation=jnp.where(
+                new_node_id >= config.max_nodes, genome.node_activation, node_activation
+            ),
+            conn_ids=jnp.where(
+                new_node_id >= config.max_nodes, genome.conn_ids, conn_ids
+            ),
+            conn_in=jnp.where(new_node_id >= config.max_nodes, genome.conn_in, conn_in),
+            conn_out=jnp.where(
+                new_node_id >= config.max_nodes, genome.conn_out, conn_out
+            ),
+            conn_weights=jnp.where(
+                new_node_id >= config.max_nodes, genome.conn_weights, conn_weights
+            ),
+            conn_enabled=jnp.where(
+                new_node_id >= config.max_nodes, genome.conn_enabled, conn_enabled
+            ),
+        ),
+        next_edge_id + 2,
+    )
+
+
+@partial(jax.jit, static_argnums=(0,))
+def mutate_add_connection(
+    config: Config, genome: Genome, key: jax.Array, next_edge_id: jax.Array
+) -> tuple[Genome, jax.Array]:
+    """Add connection"""
+    key, key_src, key_dst, key_weight = jax.random.split(key, 4)
+
+    src = pick_one(
+        key_src,
+        jnp.logical_or(
+            # input
+            jnp.logical_and(0 <= genome.node_ids, genome.node_ids < config.input_dim),
+            # or hidden
+            genome.node_ids >= config.input_dim + config.output_dim,
+        ),
+    )
+    # output or hidden
+    dst = pick_one(key_dst, genome.node_ids >= config.input_dim)
+
+    jax.debug.print("src: {}, dst: {}", src, dst)
+
+    # New graph
+    new_edge_idx = jnp.count_nonzero(genome.conn_ids >= 0)
+    conn_ids = genome.conn_ids.at[new_edge_idx].set(next_edge_id)
+    conn_in = genome.conn_in.at[new_edge_idx].set(src)
+    conn_out = genome.conn_out.at[new_edge_idx].set(dst)
+    conn_weights = genome.conn_weights.at[new_edge_idx].set(
+        jax.random.uniform(key_weight, minval=-1.0, maxval=1.0)
+    )
+    conn_enabled = genome.conn_enabled.at[new_edge_idx].set(True)
+
+    # Check
+    # - src and dest are not the same
+    valid = src != dst
+    valid = jnp.logical_and(
+        valid,
+        jnp.logical_not(
+            jnp.any(jnp.logical_and(genome.conn_in == src, genome.conn_out == dst))
+        ),
+    )
+    jax.debug.print("valid: {}", valid)
+    jax.debug.print("depth: {}", calculate_depth(config, conn_in, conn_out))
+    # - there is no cycle
+    valid = jnp.logical_and(valid, calculate_depth(config, conn_in, conn_out) != -1)
+    jax.debug.print("valid: {}", valid)
+
+    return (
+        Genome(
+            node_ids=genome.node_ids,
+            node_activation=genome.node_activation,
+            conn_ids=jnp.where(valid, conn_ids, genome.conn_ids),
+            conn_in=jnp.where(valid, conn_in, genome.conn_in),
+            conn_out=jnp.where(valid, conn_out, genome.conn_out),
+            conn_weights=jnp.where(valid, conn_weights, genome.conn_weights),
+            conn_enabled=jnp.where(valid, conn_enabled, genome.conn_enabled),
+        ),
+        jnp.where(valid, next_edge_id + 2, next_edge_id),
+    )
+
+
+def mutate_none(
+    genome: Genome, key: jax.Array, next_edge_id: jax.Array
+) -> tuple[Genome, jax.Array]:
+    return (
+        Genome(
+            node_ids=genome.node_ids,
+            node_activation=genome.node_activation,
+            conn_ids=genome.conn_ids,
+            conn_in=genome.conn_in,
+            conn_out=genome.conn_out,
+            conn_weights=genome.conn_weights,
+            conn_enabled=genome.conn_enabled,
+        ),
+        next_edge_id,
+    )
+
+
+def validate_genome(config: Config, genome: Genome) -> jax.Array:
+    """Validate the genome.
+
+    Returns: Array(shape=(), dtype=bool)"""
+    valid = calculate_depth(config, genome.conn_in, genome.conn_out) != -1
+    # TODO: Add more validation if needed.
+    return valid
+
+
+def mutate_attributes(
+    neat_config: NEATConfig, genome: Genome, key: jax.Array
+) -> Genome:
+    """Mutate the attributes of the genome.
+
+    - Change the activation function of the nodes.
+    - Change the weights of the connections.
+    - Change the enabled status of the connections.
+    """
+    # Update attributes
+    (
+        key,
+        key_update_activation_choice,
+        key_update_activation,
+        key_update_conn_weights_choice,
+        key_update_conn_weights,
+        key_update_conn_enabled_choice,
+    ) = jax.random.split(key, 6)
+    node_activations = jnp.where(
+        jnp.logical_and(
+            genome.node_ids >= 0,
+            jax.random.uniform(
+                key_update_activation_choice, genome.node_activation.shape
+            )
+            < neat_config.prob_update_activation,
+        ),
+        jax.random.randint(
+            key_update_activation,
+            genome.node_activation.shape,
+            minval=0,
+            maxval=len(ACTIVATION_FUNCTIONS),
+        ),
+        genome.node_activation,
+    )
+    conn_weights = jnp.where(
+        jnp.logical_and(
+            genome.conn_ids >= 0,
+            jax.random.uniform(
+                key_update_conn_weights_choice, genome.conn_weights.shape
+            )
+            < neat_config.prob_update_conn_weights,
+        ),
+        jax.random.uniform(
+            key_update_conn_weights, genome.conn_weights.shape, minval=-1.0, maxval=1.0
+        ),
+        genome.conn_weights,
+    )
+    conn_enabled = jnp.where(
+        jnp.logical_and(
+            genome.conn_ids >= 0,
+            jax.random.uniform(
+                key_update_conn_enabled_choice, genome.conn_enabled.shape
+            )
+            < neat_config.prob_update_conn_enabled,
+        ),
+        # Flip the connection.
+        jnp.logical_not(genome.conn_enabled),
+        genome.conn_enabled,
+    )
+    return dataclasses.replace(
+        genome,
+        node_activation=node_activations,
+        conn_weights=conn_weights,
+        conn_enabled=conn_enabled,
+    )
+
+
+def mutate_genome(
+    neat_config: NEATConfig, genome: Genome, key: jax.Array, next_edge_id: jax.Array
+) -> tuple[Genome, jax.Array]:
+    """Mutate the genome."""
+
+    key, key_choice, key_mutate = jax.random.split(key, 3)
+
+    p = jax.random.uniform(key_choice)
+
+    # TODO: Use for loop.
+    index = jnp.where(
+        p < neat_config.prob_add_node,
+        0,
+        jnp.where(
+            p < neat_config.prob_add_node + neat_config.prob_add_connection,
+            1,
+            2,
+        ),
+    )
+    jax.debug.print("p: {}, index: {}", p, index)
+
+    branches = [
+        lambda: mutate_add_node(neat_config.config, genome, key_mutate, next_edge_id),
+        lambda: mutate_add_connection(
+            neat_config.config, genome, key_mutate, next_edge_id
+        ),
+        lambda: mutate_none(genome, key_mutate, next_edge_id),
+    ]
+    # return (a, b, c)
+    (genome, new_edge_id) = jax.lax.switch(index, branches)
+
+    genome = mutate_attributes(neat_config, genome, key_mutate)
+
+    return (genome, new_edge_id)
+
+
+def generate_new_population_fn(
+    neat_config: NEATConfig, fitness: jax.typing.ArrayLike, pop: GenomePopulation
+) -> GenomePopulation:
+    """Generate a new population."""
+    # Calculate the best genomes.
+    k = math.ceil(neat_config.pop_size * neat_config.rate_keep_best)
+    _, top_idx = jax.lax.top_k(fitness, k)
+    top_genomes = jax.tree_map(lambda x: x[top_idx], pop)
+
+    # Repeat the top genomes.
+    n_repeats = (neat_config.pop_size + k - 1) // k
+    return jax.tree_map(
+        lambda x: jnp.repeat(
+            x, n_repeats, axis=0, total_repeat_length=neat_config.pop_size
+        ),
+        top_genomes,
+    )
 
 
 class NEAT(NEAlgorithm):
@@ -362,6 +705,10 @@ def test2():
     print("-------------------")
 
 
+log_dir = "./log/slimevolley"
+logger = util.create_logger(name="SlimeVolley", log_dir=log_dir, debug=True)
+
+
 def test3():
     # SlimeVolley
     task = SlimeVolley()
@@ -375,7 +722,7 @@ def test3():
         max_edges=1000,
         max_depth=10,
     )
-    neat_config = NEATConfig(config, 3)
+    neat_config = NEATConfig(config, 100)
     policy = NEATPolicy(config)
     solver = NEAT(neat_config)
 
@@ -388,19 +735,42 @@ def test3():
         solver=solver,
         train_task=train_task,
         test_task=test_task,
-        max_iter=20,
-        log_interval=4,
-        test_interval=5,
+        max_iter=10,
+        log_interval=3,
+        test_interval=2,
         n_repeats=3,
         n_evaluations=4,
         seed=42,
-        log_dir="./log",
-        logger=None,
+        log_dir=log_dir,
+        logger=logger,
     )
 
     trainer.run(demo_mode=False)
 
+    # Visualize the policy.
+    task_reset_fn = jax.jit(test_task.reset)
+    policy_reset_fn = jax.jit(policy.reset)
+    step_fn = jax.jit(test_task.step)
+    action_fn = jax.jit(policy.get_actions)
+    best_params = jax.tree.map(lambda l: l[None, :], trainer.solver.best_params)
+    key = jax.random.PRNGKey(0)[None, :]
+
+    task_state = task_reset_fn(key)
+    policy_state = policy_reset_fn(task_state)
+    screens = []
+    for _ in range(max_steps):
+        action, policy_state = action_fn(task_state, best_params, policy_state)
+        task_state, reward, done = step_fn(task_state, action)
+        screens.append(SlimeVolley.render(task_state))
+
+    gif_file = os.path.join(log_dir, "slimevolley.gif")
+    screens[0].save(
+        gif_file, save_all=True, append_images=screens[1:], duration=40, loop=0
+    )
+    logger.info("GIF saved to {}.".format(gif_file))
+
 
 if __name__ == "__main__":
-    test2()
-    test3()
+    # test2()
+    # test3()
+    pass
